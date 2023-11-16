152e3ff756727fa78a2dc610640e89f25b3f6127
Jus Jae <42322548+JusJae@users.noreply.github.com>
2023-11-16 01:18:04 +0000

    feat(add): base and index.html and css and script

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Hello World!'
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
