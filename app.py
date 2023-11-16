de2c539654d60612811d181fdbbc28c8058d5daf
Jus Jae <42322548+JusJae@users.noreply.github.com>
2023-11-16 01:28:48 +0000

    feat(add): products.html and route

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect, session
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+mongo = PyMongo(app)
+
 
 @app.route("/")
 def index():
     return render_template("base.html")
+
+
+@app.route("/products")
+def products():
+    return render_template("products.html", products=products)
 
 
 if __name__ == "__main__":
