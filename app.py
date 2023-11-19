7e08a7610240e9da3126918827ce7bc1395cd1e8
Jus Jae <42322548+JusJae@users.noreply.github.com>
2023-11-19 20:41:09 +0000

    Add product upload functionality

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import os
-from flask import Flask, flash, render_template, url_for, request, redirect, session
+from flask import Flask, flash, render_template, url_for, request, redirect
 from flask_pymongo import PyMongo
 from werkzeug.utils import secure_filename
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
+
+UPLOAD_FOLDER = 'static/images/uploaded_images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'svg', 'webp', 'heic'}
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 app = Flask(__name__)
@@ -12,26 +20,57 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 mongo = PyMongo(app)
 mongo.db = mongo.cx[app.config["MONGO_DBNAME"]]
-db = mongo.db
 
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return render_template("index.html")
+    products = mongo.db.products.find()  # type: ignore
+    return render_template("home.html", products=products)
 
 
 @app.route("/products")
 def products():
     products = mongo.db.products.find()  # type: ignore
-    return render_template("products.html", products=products)
+    return render_template("product_list.html", products=products)
 
 
-# Save the product details to MongoDB
 @app.route("/add_product", methods=["GET", "POST"])
 def add_product():
+    if request.method == "POST":
+        product_name = request.form.get('product_name')
+        product_category = request.form.get('product_category')
+        product_notes = request.form.get('product_notes')
+        date_added = request.form.get('date_added')
+
+        # Handle the image file
+        image_file = request.files.get('image_url')
+        if image_file and allowed_file(image_file.filename):
+            filename = secure_filename(image_file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            image_file.save(file_path)
+            # URL to access the image
+            image_url = f'/static/images/uploaded_images/{filename}'
+        else:
+            image_url = ''  # No image uploaded
+
+        product = {
+            "product_name": product_name,
+            "product_category": product_category,
+            "product_notes": product_notes,
+            "image_url": image_url,
+            "date_added": date_added
+        }
+
+        mongo.db.products.insert_one(product)
+        # Return the product data in the response
+        flash("Product successfully added")
+        return redirect(url_for("home"))
+
     return render_template("add_product.html")
 
 
@@ -40,8 +79,6 @@ def add_product():
 @app.route("/logout")
 @app.route("/profile")
 @app.route("/search")
-
-
 @app.route("/edit_product/<product_id>", methods=["GET", "POST"])
 def edit_product(product_id):
     # Your code here
